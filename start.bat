9796fc53fe432949d46e7431ff8635d6a1270621
SanGeJi <1940384762@qq.com>
2026-06-24 16:09:24 +0800

    add start.bat

---
+++ start.bat
@@ -0,0 +1,10 @@
+@echo off
+title Ledger
+cd /d "D:\Documents\New project\ledger"
+echo.
+echo   Ledger - full-stack accounting app
+echo   Open in browser:  http://localhost:3000
+echo   Close this window to stop.
+echo.
+node server/app.js
+pause
